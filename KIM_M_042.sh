0b19c75253fc8f39dfbc31b95c43fc68649ec8a4
Drimvort <107714721+Drimvort@users.noreply.github.com>
2022-06-17 22:12:09 +0300

    Create KIM_M_042.sh

---
+++ KIM_M_042.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+ls | grep \sh$
+mkdir Files
+cd Files
+for ((i=1; i<10; i++)) do
+echo "0"$i".txt
+0"$i".txt
+0"$i".txt
+0"$i".txt
+0"$i".txt
+0"$i".txt
+0"$i".txt
+0"$i".txt
+0"$i".txt
+0"$i".txt
+0"$i".txt" > 0$i.txt
+fallocate – l "$i"MB 0"$i".txt
+done
+for ((i=10; i<=20; i++)) do
+echo > $i.txt
+fallocate -l "$i"MB "$i".txt
+done
+ls -l
+cat 02.txt
